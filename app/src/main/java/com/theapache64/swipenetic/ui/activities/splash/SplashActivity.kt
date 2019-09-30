6947950bcd62d07b034c8aac556c11fd16693c04
theapache64 <theapache64@gmail.com>
2019-10-01 00:01:25 +0530

    ◀️ backup

--- app/src/main/java/com/theapache64/swipenetic/ui/activities/splash/SplashActivity.kt
+++ app/src/main/java/com/theapache64/swipenetic/ui/activities/splash/SplashActivity.kt
@@ -6,10 +6,15 @@ import android.os.Handler
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.github.javiersantos.appupdater.AppUpdaterUtils
+import com.github.javiersantos.appupdater.enums.AppUpdaterError
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+import com.github.javiersantos.appupdater.objects.Update
 import com.theapache64.swipenetic.R
 import com.theapache64.swipenetic.databinding.ActivitySplashBinding
 import com.theapache64.swipenetic.ui.activities.intro.IntroActivity
 import com.theapache64.swipenetic.ui.activities.main.MainActivity
+import com.theapache64.twinkill.logger.info
 import com.theapache64.twinkill.ui.activities.base.BaseAppCompatActivity
 import com.theapache64.twinkill.utils.extensions.bindContentView
 import dagger.android.AndroidInjection
@@ -17,6 +22,7 @@ import javax.inject.Inject
 
 class SplashActivity : BaseAppCompatActivity(), SplashHandler {
 
+    private lateinit var viewModel: SplashViewModel
     @Inject
     lateinit var factory: ViewModelProvider.Factory
 
@@ -25,7 +31,7 @@ class SplashActivity : BaseAppCompatActivity(), SplashHandler {
         super.onCreate(savedInstanceState)
 
         val binding = bindContentView<ActivitySplashBinding>(R.layout.activity_splash)
-        val viewModel = ViewModelProviders.of(this, factory).get(SplashViewModel::class.java)
+        this.viewModel = ViewModelProviders.of(this, factory).get(SplashViewModel::class.java)
         binding.viewModel = viewModel
         binding.handler = this
 
@@ -49,13 +55,29 @@ class SplashActivity : BaseAppCompatActivity(), SplashHandler {
 
         })
 
-        // Starting splash timer
+
+        val appUpdater = AppUpdaterUtils(this)
+            .setUpdateFrom(UpdateFrom.GITHUB)
+            .setGitHubUserAndRepo("theapache64", "swipenetic")
+            .withListener(object : AppUpdaterUtils.UpdateListener {
+                override fun onSuccess(update: Update?, isUpdateAvailable: Boolean?) {
+                    info("Update found : $update")
+                    startSplashTimer()
+                }
+
+                override fun onFailed(error: AppUpdaterError?) {
+                    info("Update failed")
+                }
+
+            })
+
+        appUpdater.start()
+    }
+
+    private fun startSplashTimer() {
         Handler().postDelayed({
             viewModel.goToNextScreen()
         }, SPLASH_DURATION)
-
-
-
     }
 
 
